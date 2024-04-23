6e820615f612c00efb8829b5729623f67fadf9c3
David <david383812@gmail.com>
2024-04-23 08:23:54 -0400

    adding analytics

--- medusajs-storefront/src/app/layout.tsx
+++ medusajs-storefront/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next"
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 import "styles/globals.css"
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
@@ -14,6 +15,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
       <body>
         <main className="relative">{props.children}</main>
         <SpeedInsights />
+        <Analytics/>
       </body>
     </html>
   )
